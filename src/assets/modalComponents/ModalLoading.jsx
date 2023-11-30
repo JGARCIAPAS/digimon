c53d5812e09a493c74cc9ca7e957c115a6d2b14e
Jorge <jorge.garcia.pastor@gmail.com>
2023-11-30 18:24:58 +0100

    FIX image route

--- src/assets/modalComponents/ModalLoading.jsx
+++ src/assets/modalComponents/ModalLoading.jsx
@@ -4,7 +4,7 @@ class ModalLoading extends Component {
   render() {
     return (
       <div className="modal-body loading-screen">
-        <img className="loading-crest" src="./src/assets/img/crest.svg" />
+        <img className="loading-crest" src="src/assets/img/crest.svg" />
         <p className="loading-text">LOADING</p>
       </div>
     );
